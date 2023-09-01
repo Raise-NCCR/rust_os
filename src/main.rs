6f0ef31b1420b448afe6039976bb0cd0caae5976
Raise-NCCR <raise1229@gmail.com>
2023-09-01 17:15:01 +0900

    vga complete

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@
 use core::panic::PanicInfo;
 
 #[panic_handler] // パニック時に呼び出す
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -20,9 +21,7 @@ static HELLO: &[u8] = b"Hello World!";
 // Cの呼び出し既約を使用するよう指定している
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    
-    #[cfg(test)]
-    test_main();
+    panic!("Some panic message");
     
     loop {}
 }
@@ -41,21 +40,4 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]");
 }
