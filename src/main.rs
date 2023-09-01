3aaecb14951c8d1befce1cefa352292025a7469f
Raise-NCCR <raise1229@gmail.com>
2023-09-01 18:14:09 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![no_std] // rust の標準ライブラリを使用しない
 #![no_main] // Rust ランタイムを使用しないので、main関数は使わない
 #![feature(custom_test_frameworks)]
-#![reexport_test_harness_main = "test_main"]
 #![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
@@ -21,23 +20,14 @@ static HELLO: &[u8] = b"Hello World!";
 // Cの呼び出し既約を使用するよう指定している
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
     
     loop {}
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
     }
 }
